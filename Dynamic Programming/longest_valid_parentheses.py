0ba1de5127a7ce5782fa0fb3816896b87ec4941c
Tanmay <16543514+95tann@users.noreply.github.com>
2019-09-01 21:06:09 -0400

    updated longest_valid_parentheses.py

--- Dynamic Programming/longest_valid_parentheses.py
+++ Dynamic Programming/longest_valid_parentheses.py
@@ -3,25 +3,22 @@ Topic   : Dynamic Programming
 Problem : Longest Valid Parentheses
 Link    : https://leetcode.com/problems/longest-valid-parentheses/
 '''
-def longest_valid_parentheses(input_string):
-    if input_string == "": return 0
-    
-    str_len = len(input_string)
-    to_balance_len, longest_len = 0, 0
+def longestValidParentheses(s):
+    if s == "": return 0
+
+    str_len = len(s)
     DP = [0] * str_len
 
-    for i in range(str_len):
-        if input_string[i] == ")" and to_balance_len > 0:
-            j = i-DP[i-1]-1
-            to_balance_len -= 1
-            
-            if input_string[j] == '(':
-                DP[i] = 2 + DP[i-1]
-                if j-1 >= 0: 
-                    DP[i] += DP[j-1]
-                longest_len = max(DP[i], longest_len)
-        
-        if input_string[i] == "(":
-            to_balance_len += 1
-    
-    return longest_len
+    for i in range(1,str_len):
+        if s[i] == ")":
+            if s[i-1] == "(":
+                DP[i] = 2 if i<2 else DP[i-2]+2
+
+            else:
+                if (i-DP[i-1]-1)>=0 and s[i-DP[i-1]-1]=="(":
+                    if i-DP[i-1]>=2:
+                        DP[i] = 2 + DP[i-1] + DP[i-DP[i-1]-2]
+                    else:
+                        DP[i] = 2 + DP[i-1]
+
+    return max(DP)
