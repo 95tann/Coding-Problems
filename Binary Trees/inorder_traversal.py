248fe06af87f7ec4116054d78d0abd40a011e35a
Tanmay <16543514+95tann@users.noreply.github.com>
2019-10-06 22:21:15 -0400

    Update inorder_traversal.py

--- Binary Trees/inorder_traversal.py
+++ Binary Trees/inorder_traversal.py
@@ -3,7 +3,31 @@ Topic   : Binary Trees
 Problem : Iterative In-order Traversal 
 Link    : https://leetcode.com/problems/binary-tree-inorder-traversal/
 '''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
+
+# Recursive Solution
+def helper(node,lst):
+    if node:
+        helper(node.left,lst)
+        lst.append(node.val)
+        helper(node.right,lst)
+
+def inorderTraversal(root):
+    if root==None:
+        return []
+    
+    result = []
+    helper(root,result)
+    return result
+
+
+# Iterative Solution
 def inorderTraversal(root):
     stack, result = [], []
     curr_node = root
