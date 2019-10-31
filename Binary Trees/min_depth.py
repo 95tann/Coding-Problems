807cdb79be57592dae40fc96c5501c013fd50d7e
Tanmay <16543514+95tann@users.noreply.github.com>
2019-10-31 01:23:21 -0400

    Create min_depth.py

---
+++ Binary Trees/min_depth.py
@@ -0,0 +1,27 @@
+'''
+Topic   : Binary Trees
+Problem : Minimum Depth of Binary Tree
+Link    : https://leetcode.com/problems/minimum-depth-of-binary-tree/
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+def minDepth(root)
+      if root == None:
+          return 0
+
+      lc, rc = root.left, root.right
+      
+      if lc and rc:
+          return 1 + min(self.minDepth(lc), self.minDepth(rc))
+      elif lc:
+          return 1 + minDepth(lc)
+      elif rc: 
+          return 1 + minDepth(rc)
+      else:
+          return 1
