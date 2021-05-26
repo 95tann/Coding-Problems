1862fcae10b9d0efd5ab5248fb0b063d25b0e27c
Tanmay <16543514+95tann@users.noreply.github.com>
2021-05-26 17:48:29 -0400

    Update jump_game.py

--- Arrays/jump_game.py
+++ Arrays/jump_game.py
@@ -13,3 +13,20 @@ def canJump(self, nums: List[int]) -> bool:
         max_reachable = max(i+nums[i], max_reachable)
 
     return True
+
+
+'''
+Topic   : Arrays & Dynamic Programming
+Problem : Jump Game II
+Link    : https://leetcode.com/problems/jump-game-ii/
+'''
+def jump(self, nums: List[int]) -> int:
+    min_jumps = [10**5 for i in range(len(nums))]
+    min_jumps[0] = 0
+
+    for i, num in enumerate(nums):
+        for jump_size in range(num+1):
+            if i+jump_size < len(nums):
+                min_jumps[i+jump_size] = min(min_jumps[i+jump_size], 1+min_jumps[i])        
+
+    return min_jumps[-1]
